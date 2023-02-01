724a4db1864109893265425b8cdc660e232c0c1f
Tibor Lovasz <104306217+thelocoman@users.noreply.github.com>
2023-02-01 13:17:48 +0100

    Add files via upload

---
+++ Egg Timer /EggTimer/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    var player: AVAudioPlayer?
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+ 
+        }
+    
+    @objc func updateCounter() {
+        if  secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed)/Float(totalTime)
+            print(Float(secondsPassed)/Float(totalTime))
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    }
+
+
+
+
+
+
